6d9f243ec0b799cc2e7bc2f97b51e94e16e6d74d
Haotian Wang <haotian.wang@duke.edu>
2018-09-05 15:12:55 -0400

    Added class Brick and Powerup

--- src/breakout/Breakout.java
+++ src/breakout/Breakout.java
@@ -4,10 +4,8 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -16,24 +14,131 @@ import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
- * First individual project, game Breakout
+ * Simple Breakout game by Haotian
  */
-public class Breakout extends Application{
-    // Initial settings for the game
-    public static final String TITLE = "Example JavaFX";
-    public static final int SIZE = 400;
+public class Breakout extends Application {
+    public static final String TITLE = "Breakout by Haotian";
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 600;
     public static final int FRAMES_PER_SECOND = 60;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     public static final Paint BACKGROUND = Color.AZURE;
     public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
     public static final String BOUNCER_IMAGE = "ball.gif";
-    public static final int BOUNCER_SPEED = 30;
-    public static final Paint MOVER_COLOR = Color.PLUM;
-    public static final int MOVER_SIZE = 50;
-    public static final int MOVER_SPEED = 5;
-    public static final Paint GROWER_COLOR = Color.BISQUE;
-    public static final double GROWER_RATE = 1.1;
-    public static final int GROWER_SIZE = 50;
+
+
+    // objects used in this game
+    private Scene myScene;
+    private Brick myBrick;
+    private Bouncer myBouncer;
+    private Powerup myPowerup;
+
+    /**
+     * Initialize what will be displayed and how it will be updated.
+     */
+    @Override
+    public void start (Stage stage) {
+        // attach scene to the stage and display it
+        myScene = setupGame(WIDTH, HEIGHT, BACKGROUND);
+        stage.setScene(myScene);
+        stage.setTitle(TITLE);
+        stage.show();
+        // attach "game loop" to timeline to play it
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        var animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    // create the game's "scene": what shapes will be in the game and their starting properties
+    private Scene setupGame (int width, int height, Paint background) {
+        // create one top level collection to organize the things in the scene
+        var root = new Group();
+        // create a place to see the shapes
+        var scene = new Scene(root, width, height, background);
+        // make some shapes and set their properties
+        var image = new Image(this.getClass().getClassLoader().getResourceAsStream(BOUNCER_IMAGE));
+        var myBouncer = new Bouncer(image, width, height);
+        root.getChildren().add(myBouncer.getView());
+        myMover = new Rectangle(width / 2 - 25, height / 2 - 100, MOVER_SIZE, MOVER_SIZE);
+        myMover.setFill(MOVER_COLOR);
+        myGrower = new Rectangle(width / 2 - 25, height / 2 + 50, GROWER_SIZE, GROWER_SIZE);
+        myGrower.setFill(GROWER_COLOR);
+        // order added to the group is the order in which they are drawn
+        root.getChildren().add(myMover);
+        root.getChildren().add(myGrower);
+        // respond to input
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+        scene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
+        return scene;
+    }
+
+    // Change properties of shapes to animate them
+    // Note, there are more sophisticated ways to animate shapes, but these simple ways work fine to start.
+    private void step (double elapsedTime) {
+        // update attributes
+        myBouncer.move(elapsedTime);
+        myMover.setRotate(myMover.getRotate() - 1);
+        myGrower.setRotate(myGrower.getRotate() + 1);
+
+        // check for collisions
+        // with shapes, can check precisely
+        var intersect = Shape.intersect(myMover, myGrower);
+        if (intersect.getBoundsInLocal().getWidth() != -1) {
+            myMover.setFill(HIGHLIGHT);
+        }
+        else {
+            myMover.setFill(MOVER_COLOR);
+        }
+        // with images can only check bounding box
+        for (Bouncer b : myBouncers) {
+            if (myGrower.getBoundsInParent().intersects(b.getView().getBoundsInParent())) {
+                myGrower.setFill(HIGHLIGHT);
+            }
+        }
+
+        // bounce off all the walls
+        for (Bouncer b : myBouncers) {
+            b.bounce(myScene.getWidth(), myScene.getHeight());
+        }
+    }
+
+    // What to do each time a key is pressed
+    private void handleKeyInput (KeyCode code) {
+        if (code == KeyCode.RIGHT) {
+            myMover.setX(myMover.getX() + MOVER_SPEED);
+        }
+        else if (code == KeyCode.LEFT) {
+            myMover.setX(myMover.getX() - MOVER_SPEED);
+        }
+        else if (code == KeyCode.UP) {
+            myMover.setY(myMover.getY() - MOVER_SPEED);
+        }
+        else if (code == KeyCode.DOWN) {
+            myMover.setY(myMover.getY() + MOVER_SPEED);
+        }
+    }
+
+    // What to do each time a key is pressed
+    private void handleMouseInput (double x, double y) {
+        if (myGrower.contains(x, y)) {
+            myGrower.setScaleX(myGrower.getScaleX() * GROWER_RATE);
+            myGrower.setScaleY(myGrower.getScaleY() * GROWER_RATE);
+        }
+    }
+
+
+    /**
+     * Start the program.
+     */
+    public static void main (String[] args) {
+        launch(args);
+    }
 }
